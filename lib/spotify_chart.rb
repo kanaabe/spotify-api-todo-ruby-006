688963e82c8d8122468081f78d65a7ebe21b472a
kanaabe <kanakoabe5@gmail.com>
2014-10-08 14:35:40 -0400

    complete spotify lab

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -6,16 +6,19 @@ class SpotifyChart
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
-    # return a string that is the base url + / + preference + / + region + / + latest
+    @base_url + preference + "/" + region + "/latest"
   end
 
   def get_json(url)
-    # load json given a url here
-    # refer to the references if you have questions about this
+    #@json_url = url
+    #doc = Nokogiri::HTML(open(url))
+    json_hash = JSON.load(open(url))
+
+
   end
 
   def fetch_track_album_artist(music_hash)
@@ -39,30 +42,35 @@ class SpotifyChart
 
     # given a hash, like the one above, this method should return a string like:
     # '<track name>' by <artist name> from the <album name>
-  
+    
     # the track name, artist name, and album name should be the first track in the
     # tracks array
+    track_hash = music_hash["tracks"][0]
+    "'#{track_hash["track_name"]}' by #{track_hash["artist_name"]} from the album #{track_hash["album_name"]}"
+
   end
 
 
   def most_streamed(region)
     # call no get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
-
+    url = get_url("most_streamed",region)
     # call on get_json here, using the string that get_url returns
-    
+    json = get_json(url)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+    return fetch_track_album_artist(json)
   end
 
   def most_shared(region)
     # call no get_url here, where preference is the string 'most_shared',
     # and set it equal to a variable
-
+    url = get_url("most_shared",region)
     # call on get_json here, using the string that get_url returns
-    
+    json = get_json(url)
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+    return fetch_track_album_artist(json)
   end
 
 end
